ad6a0d11034fdf17b07144b3bd4d2b08407711e1
rajzon <dawid.nosek@microsoft.wsei.edu.pl>
2020-10-17 22:41:22 +0200

    added migration for seeding StockOnHold records

--- GameShop.Infrastructure/Migrations/20201017203711_StockOnHoldSeed.Designer.cs
+++ GameShop.Infrastructure/Migrations/20201017203711_StockOnHoldSeed.Designer.cs
@@ -4,14 +4,16 @@ using GameShop.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GameShop.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201017203711_StockOnHoldSeed")]
+    partial class StockOnHoldSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GameShop.Infrastructure/Migrations/20201017203711_StockOnHoldSeed.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GameShop.Infrastructure.Migrations
+{
+    public partial class StockOnHoldSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Stocks",
+                columns: new[] { "Id", "ProductId", "Quantity" },
+                values: new object[,]
+                {
+                    { 1, 1, 10 },
+                    { 2, 2, 25 },
+                    { 3, 3, 1 },
+                    { 4, 4, 30 },
+                    { 5, 5, 60 },
+                    { 6, 6, 5 },
+                    { 7, 7, 120 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Stocks",
+                keyColumn: "Id",
+                keyValue: 7);
+        }
+    }
+}
